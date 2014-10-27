bd1a76dd363eba1b0167cb7349cd0e96e5468f28
Philipp Meier <phimei@gmx.ch>
2014-10-27 22:44:26 +0100

    Added some details to BSONFormatsBM

--- app/extensions/BSONFormatsBM.scala
+++ app/extensions/BSONFormatsBM.scala
@@ -4,11 +4,17 @@ import play.api.libs.json._
 import play.modules.reactivemongo.json.BSONFormats.PartialFormat
 import reactivemongo.bson.{BSONValue, BSONObjectID}
 
+import scala.util.Try
+
 object BSONFormatsBM {
 
   implicit object BSONObjectIDFormat extends PartialFormat[BSONObjectID] {
     def partialReads: PartialFunction[JsValue, JsResult[BSONObjectID]] = {
-      case JsObject(("oid", JsString(v)) +: Nil) => JsSuccess(BSONObjectID(v))
+      case JsObject(("oid", JsString(v)) +: Nil) =>
+        val maybeOID: Try[BSONObjectID] = BSONObjectID.parse(v)
+        if(maybeOID.isSuccess) JsSuccess(maybeOID.get) else {
+          JsError("Expected BSONObjectID as JsString in field oid")
+        }
     }
 
     val partialWrites: PartialFunction[BSONValue, JsValue] = {
