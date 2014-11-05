d16467b74df87d0c9a86ee7184387d12d388988a
Philipp Meier <phimei@gmx.ch>
2014-11-05 23:45:28 +0100

    Had to add $ Sign for IDs again becaus of compatibility problems with MongoDB

--- app/extensions/BSONFormatsBM.scala
+++ app/extensions/BSONFormatsBM.scala
@@ -8,17 +8,20 @@ import scala.util.Try
 
 object BSONFormatsBM {
 
+
   implicit object BSONObjectIDFormat extends PartialFormat[BSONObjectID] {
     def partialReads: PartialFunction[JsValue, JsResult[BSONObjectID]] = {
-      case JsObject(("$oid", JsString(v)) +: Nil) =>
+      case JsObject(("$oid", JsString(v)) +: Nil) => {
         val maybeOID: Try[BSONObjectID] = BSONObjectID.parse(v)
-        if(maybeOID.isSuccess) JsSuccess(maybeOID.get) else {
+        if (maybeOID.isSuccess) JsSuccess(maybeOID.get)
+        else {
           JsError("Expected BSONObjectID as JsString in field oid")
         }
+      }
     }
 
     val partialWrites: PartialFunction[BSONValue, JsValue] = {
-      case oid: BSONObjectID => Json.obj("oid" -> oid.stringify)
+      case oid: BSONObjectID => Json.obj("$oid" -> oid.stringify)
     }
   }
 
